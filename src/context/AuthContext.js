eec70612fabc14557034e68047cb93a6dace9839
Naoya Ono <ono.naoyaa@gmail.com>
2019-08-14 15:46:43 +0800

    Add signin functionality

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -7,7 +7,7 @@ const authReducer = (state, action) => {
   switch (action.type) {
     case 'add_error':
       return { ...state, errorMessage: action.payload };
-    case 'signup':
+    case 'signin':
       return { token: action.payload, errorMessage: '' };
     default:
       return state;
@@ -18,7 +18,7 @@ const signup = dispatch => async ({ email, password }) => {
   try {
     const response = await trackerApi.post('/signup', { email, password });
     await AsyncStorage.setItem('token', response.data.token);
-    dispatch({ type: 'signup', payload: response.data.token });
+    dispatch({ type: 'signin', payload: response.data.token });
     navigate('TrackList');
   } catch (err) {
     dispatch({
@@ -28,7 +28,19 @@ const signup = dispatch => async ({ email, password }) => {
   }
 };
 
-const signin = dispatch => ({ email, password }) => dispatch();
+const signin = dispatch => async ({ email, password }) => {
+  try {
+    const response = await trackerApi.post('/signin', { email, password });
+    await AsyncStorage.setItem('token', response.data.token);
+    dispatch({ type: 'signin', payload: response.data.token });
+    navigate('TrackList');
+  } catch (err) {
+    dispatch({
+      type: 'add_error',
+      payload: 'Something went wrong with signin',
+    });
+  }
+};
 
 const signout = dispatch => () => dispatch();
 
--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import AuthForm from '../components/AuthForm';
+import NavLink from '../components/NavLink';
+import { Context as AuthContext } from '../context/AuthContext';
 
 const SigninScreen = () => {
+  const { state, signin } = useContext(AuthContext);
+
   return (
-    <View>
-      <Text style={{ fontSize: 48 }}>SigninScreen</Text>
+    <View style={styles.container}>
+      <AuthForm
+        headerText="Sign In to Your Account"
+        errorMessage={state.errorMessage}
+        onSubmit={signin}
+        submitButtonText="Sign In"
+      />
+      <NavLink
+        text="Don't have an account> Sign up instead"
+        routeName="Signup"
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+SigninScreen.navigationOptions = {
+  header: null,
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 250,
+  },
+});
 
 export default SigninScreen;
