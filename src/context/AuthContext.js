e2095d8a8e6b72e4de02094be4ddb3e2787c0826
Naoya Ono <ono.naoyaa@gmail.com>
2019-08-14 15:09:04 +0800

    Store token to async storage

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,3 +1,4 @@
+import { AsyncStorage } from 'react-native';
 import createDataContext from './createDataContext';
 import trackerApi from '../api/tracker';
 
@@ -5,6 +6,8 @@ const authReducer = (state, action) => {
   switch (action.type) {
     case 'add_error':
       return { ...state, errorMessage: action.payload };
+    case 'signup':
+      return { token: action.payload, errorMessage: '' };
     default:
       return state;
   }
@@ -13,7 +16,8 @@ const authReducer = (state, action) => {
 const signup = dispatch => async ({ email, password }) => {
   try {
     const response = await trackerApi.post('/signup', { email, password });
-    console.log(response.data);
+    await AsyncStorage.setItem('token', response.data.token);
+    dispatch({ type: 'signup', payload: response.data.token });
   } catch (err) {
     dispatch({
       type: 'add_error',
@@ -29,5 +33,5 @@ const signout = dispatch => () => dispatch();
 export const { Provider, Context } = createDataContext(
   authReducer,
   { signup, signin, signout },
-  { isSignedIn: false, errorMessage: '' }
+  { token: null, errorMessage: '' }
 );
