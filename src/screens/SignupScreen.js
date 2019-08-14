0abc65726adbb4f3bc08db34ca5b7effc4df75ec
Naoya Ono <ono.naoyaa@gmail.com>
2019-08-14 13:53:53 +0800

    Install react-native-elements

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { Button, Input, Text } from 'react-native-elements';
+import Spacer from '../components/Spacer';
 
-const SignupScreen = () => {
+const SignupScreen = ({ navigation }) => {
   return (
-    <View>
-      <Text style={{ fontSize: 48 }}>SignupScreen</Text>
-    </View>
+    <>
+      <Spacer>
+        <Text h3>Sign Up for Tracker</Text>
+      </Spacer>
+      <Input label="Email" />
+      <Spacer />
+      <Input label="Password" />
+      <Spacer>
+        <Button title="Sign Up" />
+      </Spacer>
+    </>
   );
 };
 
