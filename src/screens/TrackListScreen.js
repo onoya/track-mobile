af19aa4e0b74c5909d4fb4f55fb4b8999608ed3b
Naoya Ono <ono.naoyaa@gmail.com>
2019-08-15 00:51:24 +0800

    Display track list

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import { Button, StyleSheet, Text } from 'react-native';
+import React, { useContext } from 'react';
+import {
+  Button,
+  StyleSheet,
+  Text,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
+import { NavigationEvents } from 'react-navigation';
+import { ListItem } from 'react-native-elements';
+import { Context as TrackContext } from '../context/TrackContext';
 
 const TrackListScreen = ({ navigation }) => {
+  const { state, fetchTracks } = useContext(TrackContext);
+
   return (
     <>
+      <NavigationEvents onWillFocus={fetchTracks} />
       <Text style={{ fontSize: 48 }}>TrackListScreen</Text>
-      <Button
-        title="Go to Track Detail"
-        onPress={() => navigation.navigate('TrackDetail')}
+      <FlatList
+        data={state}
+        keyExtractor={item => item._id}
+        renderItem={({ item }) => (
+          <TouchableOpacity>
+            <ListItem chevron title={item.name} />
+          </TouchableOpacity>
+        )}
       />
     </>
   );
